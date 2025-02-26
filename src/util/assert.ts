a48b230f71369132957dffeb8bd7e5b905f0bee8
Björn <41452212+bjoern621@users.noreply.github.com>
2025-02-26 12:06:24 +0100

    new assertions

--- src/util/assert.ts
+++ src/util/assert.ts
@@ -1,3 +1,20 @@
+class AssertionError extends Error {
+    constructor(msg?: string) {
+        super("[ASSERT] " + (msg || ""));
+    }
+}
+
 export function assert(expr: unknown, msg?: string): asserts expr {
-    if (!expr) throw new Error(msg);
+    if (!expr) throw new AssertionError(msg);
+}
+
+export function isUndefined(
+    obj: unknown,
+    msg?: string
+): asserts obj is undefined {
+    if (obj !== undefined) throw new AssertionError(msg);
+}
+
+export function never(msg?: string, _exp: boolean = false): asserts _exp {
+    throw new AssertionError(msg);
 }
