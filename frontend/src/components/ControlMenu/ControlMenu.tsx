2721a404d986e180187b7273846bc5b552b5b379
Björn <41452212+bjoern621@users.noreply.github.com>
2025-04-04 17:24:31 +0200

    blue outline for every button

--- frontend/src/components/ControlMenu/ControlMenu.tsx
+++ frontend/src/components/ControlMenu/ControlMenu.tsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import css from "./ControlMenu.module.css";
+import css from "./ControlMenu.module.scss";
 import errorAsValue from "../../util/ErrorAsValue";
 
 type ControlMenuProps = {
