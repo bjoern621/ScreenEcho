fe1408d6a4229b4eb69c022a70e556fb3e8651ff
Björn <41452212+bjoern621@users.noreply.github.com>
2025-04-07 13:19:09 +0200

    refactored package connection; added writeMutex

---
+++ backend/connection/conn.go
@@ -0,0 +1,62 @@
+package connection
+
+import (
+	"log"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type Conn struct {
+	socket             *websocket.Conn
+	closeHandlers      []func()
+	closeHandlersMutex sync.RWMutex
+	// From https://pkg.go.dev/github.com/gorilla/websocket#hdr-Concurrency: Connections support one concurrent reader and one concurrent writer.
+	writeMutex sync.Mutex
+}
+
+// AddCloseHandler registers a function to be called when the WebSocket connection is closed.
+//
+// Multiple close handlers can be added, and they will all be executed after the connection was closed.
+// However, there is no RemoveCloseHandler function, so once a handler is registered, it cannot be removed.
+//
+// When the WebSocket connection is closed, no more messages will be read or written.
+// The connection is considered invalid, and any pointers to Conn should be freed to avoid memory leaks.
+func (conn *Conn) AddCloseHandler(handler func()) {
+	conn.closeHandlers = append(conn.closeHandlers, handler)
+}
+
+// SendMessage sends a typed message over a WebSocket connection.
+//
+// Example usage:
+//
+//	var conn *websocket.Conn
+//
+//	// Create a typed message
+//
+//	type InfoMessage struct {
+//	    Information string `json:"info"`
+//	}
+//
+//	message := TypedMessage[InfoMessage]{
+//	    Type: "info-msg",
+//	    Msg:  InfoMessage{Information: "here is your new information"},
+//	}
+//
+//	// Send the message
+//
+//	err = SendMessage(conn, message)
+//	if err != nil {
+//	    log.Printf("Failed to send message: %v", err)
+//	} else {
+//	    log.Println("Message sent successfully")
+//	}
+func SendMessage[T any](conn *Conn, msg TypedMessage[T]) error {
+	conn.writeMutex.Lock()
+	defer conn.writeMutex.Unlock()
+
+	log.Printf("msg sent: %v", msg)
+
+	err := conn.socket.WriteJSON(msg)
+	return err
+}
